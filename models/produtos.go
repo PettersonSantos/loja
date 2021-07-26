a9b4c602b5a88effa01c7855558283cbb0b3fadd
PettersonSantos <petterson.santos@outlook.com.br>
2021-07-26 16:18:17 -0300

    terminando a implementação do crud

--- models/produtos.go
+++ models/produtos.go
@@ -3,7 +3,7 @@ package models
 import "loja/config"
 
 type Produto struct {
-	id         int
+	Id         int
 	Nome       string
 	Descricao  string
 	Preco      float64
@@ -13,7 +13,7 @@ type Produto struct {
 func FindAll() []Produto {
 	db := config.ConectaDB()
 
-	findAll, err := db.Query("Select * from produtos")
+	findAll, err := db.Query("Select * from produtos order by id asc")
 
 	if err != nil {
 		panic(err.Error())
@@ -32,6 +32,7 @@ func FindAll() []Produto {
 		if err != nil {
 			panic(err.Error())
 		}
+		p.Id = id
 		p.Nome = nome
 		p.Preco = preco
 		p.Descricao = descricao
@@ -53,6 +54,59 @@ func Save(nome, descricao string, preco float64, quantidade int) {
 	}
 
 	insert.Exec(nome, descricao, preco, quantidade)
+
+	defer db.Close()
+}
+
+func Delete(id string) {
+	db := config.ConectaDB()
+	delete, err := db.Prepare("Delete from produtos where id = $1")
+	if err != nil {
+		panic(err.Error())
+	}
+	delete.Exec(id)
+	defer db.Close()
+}
+
+func FindById(id string) Produto {
+	db := config.ConectaDB()
+
+	findById, err := db.Query("Select * from produtos where id = $1", id)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	produto := Produto{}
+	for findById.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err = findById.Scan(&id, &descricao, &preco, &quantidade, &nome)
+
+		if err != nil {
+			panic(err.Error())
+		}
+		produto.Id = id
+		produto.Nome = nome
+		produto.Preco = preco
+		produto.Descricao = descricao
+		produto.Quantidade = quantidade
+	}
+	defer db.Close()
+	return produto
+}
+
+func Update(id int, nome, descricao string, preco float64, quantidade int)  {
+	db := config.ConectaDB()
+	insert, err := db.Prepare("UPDATE produtos SET nome = $1, descricao = $2, preco = $3, quantidade = $4 WHERE id = $5  ")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	insert.Exec(nome, descricao, preco, quantidade, id)
 
 	defer db.Close()
 }
